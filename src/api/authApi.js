929684a8079db8c7398d3768c36ce28324ab1f79
behdin123 <behdin2010@gmail.com>
2023-05-23 15:22:42 +0200

    first commit

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -6,8 +6,6 @@ export default {
 // Register a new user
 register: async (data) => {
     try {
-        
-      console.log("Request body:", data);
       const response = await axios.post(`https://pwa-rest-api-mevn.onrender.com/auth/register`, data);  
      
       return response;
@@ -20,11 +18,10 @@ register: async (data) => {
   // Login a user
   login: async (data) => {
     try {
-        
-      console.log("Request body:", data);
       const response = await axios.post(`https://pwa-rest-api-mevn.onrender.com/auth/login`, data);  //${BASE_URL}
-     
+      
       return response;
+
     } catch (error) {
       throw error;
     }
