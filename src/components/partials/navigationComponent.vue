bf5c66bdd9c5b1d69eb48639c6a3f51674187dc2
behdin123 <behdin2010@gmail.com>
2023-05-29 15:54:07 +0200

    sixth commit

--- src/components/partials/navigationComponent.vue
+++ src/components/partials/navigationComponent.vue
@@ -61,14 +61,14 @@ const props = defineProps({
   toggleDarkMode: Function,
 });
 
-/* const router = useRouter();
+const router = useRouter();
 
 //checking if the user is logged out then it should be send back to the frontpage
 watch(isLoggedIn, (newIsLoggedIn) => {
   if (!newIsLoggedIn) {
     router.push('/');
   }
-}); */
+});
 
 
 </script>
